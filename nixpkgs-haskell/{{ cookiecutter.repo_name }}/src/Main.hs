ba9efbeda81e6e015b30b5bf44d5c4ecc5cab90c
Stefan Matting <stefan.matting@gmail.com>
2020-09-15 15:55:08 +0200

    remove old nixpkgs

--- nixpkgs-haskell/{{ cookiecutter.repo_name }}/src/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main
-
-where
-
-import qualified {{ cookiecutter.module_root }}.Lib as Lib
-
-main = Lib.main
--- nixpkgs-haskell/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.module_root }}/Lib.hs
+++
@@ -1,5 +0,0 @@
-module {{ cookiecutter.module_root }}.Lib where
-
-haskell = "awesome"
-
-main = putStrLn "Hello, World!"
